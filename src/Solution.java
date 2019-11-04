1239bbf11bed16b457d5ba9d8573f2e7a97df7a4
Your Name <you@example.com>
2019-11-04 11:53:38 +0800

    git push Solution101

--- src/Solution.java
+++ src/Solution.java
@@ -3,7 +3,5 @@ public class Solution {
        Solution35 solution35=new Solution35();
        int res=solution35.searchInsert(new int[]{1, 3, 5, 6}, 4);
          System.out.println(res);
-         System.out.println("sssssssssssssss");
-         System.out.println("sssssssssssssssssssssssssssssssssssssss");
     }
 }
