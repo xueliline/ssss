dbb15abe197c3d0c329d58b52ac719877d9f25fd
Your Name <you@example.com>
2019-11-04 14:03:07 +0800

    git push Solution101

---
+++ src/Solution540.java
@@ -0,0 +1,23 @@
+public class Solution540 {
+    public int singleNonDuplicate(int[] nums) {
+        int l=0;
+        int h=nums.length-1;
+        while (l<=h)
+        {
+
+            int mid=(h+l)/2;
+            System.out.println(l+"    "+mid);
+            if(nums[mid]==nums[mid-1])
+                mid--;
+            if(nums[mid])
+            return nums[mid];
+            if()
+        }
+       return nums[l];
+    }
+    public static void main(String[] args) {
+      Solution540 solution540=new Solution540();
+          int res=solution540.singleNonDuplicate(new int []{1,1,2,3,3,4,4,8,8});
+        System.out.println(res);
+    }
+}
