cfb9acb6d484100822234c1b849ee22a57374546
Your Name <you@example.com>
2019-11-04 10:27:53 +0800

    git pull

--- src/Solution.java
+++ src/Solution.java
@@ -2,5 +2,6 @@ public class Solution {
      public static void main(String[] args) {
          System.out.println("我学会了项目控制");
          System.out.println("dddddddddddd");
+         System.out.println("我咋这么笨呢");
     }
 }
